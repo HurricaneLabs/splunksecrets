c40a335f629f5c62fc6bec6abfebb8e378e1c0fe
Steve McMaster <mcmaster@hurricanelabs.com>
2018-10-24 12:54:53 -0400

    Add support for new encryption in Splunk 7.2. Thanks to nkadri@splunk.com for the bug report

--- splunksecrets.py
+++ splunksecrets.py
@@ -4,31 +4,60 @@ import argparse
 import base64
 import getpass
 import itertools
+import os
 
 import six
 from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives.ciphers import algorithms, Cipher
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.ciphers import algorithms, Cipher, modes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
 
-def decrypt(key, ciphertext):
+def decrypt(secret, ciphertext):
     """Given the first 16 bytes of splunk.secret, decrypt a Splunk password"""
+    plaintext = None
     if ciphertext.startswith("$1$"):
         ciphertext = base64.b64decode(ciphertext[3:])
+        key = secret[:16]
 
-    algorithm = algorithms.ARC4(key)
-    cipher = Cipher(algorithm, mode=None, backend=default_backend())
-    decryptor = cipher.decryptor()
-    plaintext = decryptor.update(ciphertext)
+        algorithm = algorithms.ARC4(key)
+        cipher = Cipher(algorithm, mode=None, backend=default_backend())
+        decryptor = cipher.decryptor()
+        plaintext = decryptor.update(ciphertext)
 
-    chars = []
-    for char1, char2 in zip(plaintext[:-1], itertools.cycle("DEFAULTSA")):
-        chars.append(six.byte2int([char1]) ^ ord(char2))
+        chars = []
+        for char1, char2 in zip(plaintext[:-1], itertools.cycle("DEFAULTSA")):
+            chars.append(six.byte2int([char1]) ^ ord(char2))
 
-    return "".join([six.unichr(c) for c in chars])
+        plaintext = "".join([six.unichr(c) for c in chars])
+    elif ciphertext.startswith("$7$"):
+        ciphertext = base64.b64decode(ciphertext[3:])
+
+        kdf = PBKDF2HMAC(
+            algorithm=hashes.SHA256(),
+            length=32,
+            salt=b"disk-encryption",
+            iterations=1,
+            backend=default_backend()
+        )
+        key = kdf.derive(secret)
+
+        iv = ciphertext[:16]  # pylint: disable=invalid-name
+        tag = ciphertext[-16:]
+        ciphertext = ciphertext[16:-16]
+
+        algorithm = algorithms.AES(key)
+        cipher = Cipher(algorithm, mode=modes.GCM(iv, tag), backend=default_backend())
+        decryptor = cipher.decryptor()
+        plaintext = decryptor.update(ciphertext).decode()
+
+    return plaintext
 
 
-def encrypt(key, plaintext):
+def encrypt(secret, plaintext):
     """Given the first 16 bytes of splunk.secret, encrypt a Splunk password"""
+    key = secret[:16]
+
     chars = []
     for char1, char2 in zip(plaintext, itertools.cycle("DEFAULTSA")):
         chars.append(ord(char1) ^ ord(char2))
@@ -44,15 +73,37 @@ def encrypt(key, plaintext):
     return "$1$%s" % base64.b64encode(ciphertext).decode()
 
 
+def encrypt_new(secret, plaintext, iv=None):  # pylint: disable=invalid-name
+    """Use the new AES 256 GCM encryption in Splunk 7.2"""
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=b"disk-encryption",
+        iterations=1,
+        backend=default_backend()
+    )
+    key = kdf.derive(secret)
+
+    if iv is None:
+        iv = os.urandom(16)
+
+    algorithm = algorithms.AES(key)
+    cipher = Cipher(algorithm, mode=modes.GCM(iv), backend=default_backend())
+    encryptor = cipher.encryptor()
+    ciphertext = encryptor.update(plaintext.encode()) + encryptor.finalize()
+
+    return "$7$%s" % base64.b64encode(b"%s%s%s" % (iv, ciphertext, encryptor.tag)).decode()
+
 def main():  # pragma: no cover
     """Command line interface"""
     cliargs = argparse.ArgumentParser()
     cliargs.add_argument("--splunk-secret", required=True)
     cliargs.add_argument("-D", "--decrypt", action="store_const", dest="mode", const="decrypt")
+    cliargs.add_argument("--new", action="store_const", dest="mode", const="encrypt_new")
     args = cliargs.parse_args()
 
     with open(args.splunk_secret, "rb") as splunk_secret_file:
-        key = splunk_secret_file.read()[:16]
+        key = splunk_secret_file.read().strip()
 
     if args.mode == "decrypt":
         try:
@@ -67,4 +118,7 @@ def main():  # pragma: no cover
         except KeyboardInterrupt:
             pass
         else:
-            print(encrypt(key, plaintext))
+            if args.mode == "encrypt_new":
+                print(encrypt_new(key, plaintext))
+            else:
+                print(encrypt(key, plaintext))
--- tests.py
+++ tests.py
@@ -20,8 +20,19 @@ class TestSplunkSecrets(unittest.TestCase):
         ciphertext = splunksecrets.encrypt(splunk_secret[:16], "temp1234")
         self.assertEqual(ciphertext, "$1$n6g0W7F51ZAK")
 
+    def test_encrypt_new(self):
+        ciphertext = splunksecrets.encrypt_new(splunk_secret, "temp1234", iv=b"i5dKMGaSIRNpJty4")
+        self.assertEqual(ciphertext, "$7$aTVkS01HYVNJUk5wSnR5NIu4GXLhj2Qd49n2B6Y8qmA/u1CdL9JYxQ==")
+
     def test_decrypt(self):
         plaintext = splunksecrets.decrypt(splunk_secret[:16], "$1$n6g0W7F51ZAK")
+        self.assertEqual(plaintext, "temp1234")
+
+    def test_decrypt_new(self):
+        plaintext = splunksecrets.decrypt(
+            splunk_secret,
+            "$7$aTVkS01HYVNJUk5wSnR5NIu4GXLhj2Qd49n2B6Y8qmA/u1CdL9JYxQ=="
+        )
         self.assertEqual(plaintext, "temp1234")
 
     def test_end_to_end(self):
@@ -29,4 +40,11 @@ class TestSplunkSecrets(unittest.TestCase):
         plaintext1 = base64.b64encode(os.urandom(255))[:24].decode()
         ciphertext = splunksecrets.encrypt(splunk_secret[:16], plaintext1)
         plaintext2 = splunksecrets.decrypt(splunk_secret[:16], ciphertext)
+        self.assertEqual(plaintext2, plaintext1)
+
+    def test_end_to_end(self):
+        splunk_secret = base64.b64encode(os.urandom(255))[:255]
+        plaintext1 = base64.b64encode(os.urandom(255))[:24].decode()
+        ciphertext = splunksecrets.encrypt_new(splunk_secret, plaintext1)
+        plaintext2 = splunksecrets.decrypt(splunk_secret, ciphertext)
         self.assertEqual(plaintext2, plaintext1)
